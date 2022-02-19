ccee14d131a66bfd4d6192871e2a6f3ade4b79d0
William Low <wlwleong@gmail.com>
2022-02-19 23:24:02 +0800

    ft_printf.h normed

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: wlwleong <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 17:43:36 by wlwleong          #+#    #+#             */
-/*   Updated: 2022/02/19 17:46:25 by wlwleong         ###   ########.fr       */
+/*   Updated: 2022/02/19 23:22:37 by wlwleong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 void	ft_putchar(char c);
 void	ft_putstr(char *s);
 void	ft_putnbr_base(unsigned long int nbr, int n_base, char *base);
-int     ft_char_count(int flag);
-int     ft_printf(const char *in_str, ...);
+int		ft_char_count(int flag);
+int		ft_printf(const char *in_str, ...);
 
 #endif
